b6b03c7491ef91ae29cd359151e9ff4fb8c6467c
oxforward <oxforward@163.com>
2015-05-11 22:49:52 +0800

    Interrupt
    Notes-language-en

--- exam3.c
+++ exam3.c
@@ -1,21 +1,22 @@
 /*************************************
-*名称：中断实验
-*功能：P0口数码管循环显示0～8
-*	   外中断0、外中断1发生时分别在P2口、P1口依次显示0～8
-*	   过实验可演示高优先级可中断低优先级，但低优先级的中断请求不能中断高优先级
+*Name:		Interrupt experiment
+*Function:	P0 digital tube display cycle of 0 to 8
+*	   		When Interrupt 0,Interrupt 1 occurs,P2、P1 in order to show from 0 to 8
+*	   		Through the experiment demonstrates the high priority interrupt lower priority, 
+*	   		but low priority interrupt request not high priority interrupt
 *************************************/
 
 #include<reg52.h>
 #define uchar unsigned char
 #define uint unsigned int
 
-uchar code table[]={			//共阳极数码管编码表
+uchar code table[]={			//Common anode LED code table
 0xc0, 0xf9, 0xa4, 0xb0,
 0x99, 0x92, 0x82, 0xf8,
 0x80, 0x90, 0x88, 0x83,
 0xc6, 0xa1, 0x86, 0x8e};
 
-uchar code seg_table[]={		//共阴极数码管编码表
+uchar code seg_table[]={		//Common cathode LED code table 
 0x3f, 0x06, 0x5b, 0x4f,
 0x66, 0x6d, 0x7d, 0x07,
 0x7f, 0x6f, 0x77, 0x7c,
@@ -40,17 +41,17 @@ void main()
 void stop_init()
 {
 	IT0 = 1;
-	EX0 = 1;	//允许外部中断0开关	
+	EX0 = 1;	//Interrupt 0 switch enable	
 	IT1 = 1;
-	EX1 = 1;	//允许外部中断1开关	
-//	PX0 = 0;	//设置中断优先级:高优先级的可以打断低优先级的
+	EX1 = 1;	//Interrupt 1 switch enable	
+//	PX0 = 0;	//Set interrupt priority: high priority interrupt lower priority
 //	PX1	= 1;	
-	PX0 = 1;	//设置中断优先级:高优先级的可以打断低优先级的
+	PX0 = 1;	//Set interrupt priority: high priority interrupt lower priority
 	PX1 = 0;	
-	EA = 1;		//中断总开关	
+	EA = 1;		//Interrupt switch	
 }
 
-void delay_on(uint delay_time)		//延时函数
+void delay_on(uint delay_time)		//Delay function
 {
 	int i;
 	int j;
@@ -58,7 +59,7 @@ void delay_on(uint delay_time)		//延时函数
 		for(j=0; j<=delay_time; j++);
 }
 
-void outside_stop0() interrupt 0	//外部中断0处理函数
+void outside_stop0() interrupt 0	//Interrupt 0 Processing function
 {
 	int num;
 	for(num=0; num<=7; num++){
@@ -69,7 +70,7 @@ void outside_stop0() interrupt 0	//外部中断0处理函数
 		
 }
 
-void outside_stop1() interrupt 2	//外部中断1的处理程序
+void outside_stop1() interrupt 2	//Interrupt 1 Processing function
 {
 	int num;
 	for(num=0; num<=7; num++){
